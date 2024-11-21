53fd6e6a33524cabd67c57a638342a58feb4b619
Ian McLerran <imclerran@protonmail.com>
2024-11-21 14:30:02 -0600

    adjust rate limit on login attemps depending on whether in production environment

--- src/middleware/ratelimit.middleware.js
+++ src/middleware/ratelimit.middleware.js
@@ -1,9 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
+const maxReqs = process.env.NODE_ENV !== 'production' ? 500 : 5;
+
 // Define rate-limiting settings for login endpoint
 const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 login requests per window
+  max: maxReqs, // Limit each IP to 5 login requests per window, 500 in development
   message: 'Too many login attempts. Please try again later.',
   standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
   legacyHeaders: false,  // Disable `X-RateLimit-*` headers
