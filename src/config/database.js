860233ab1c72e5d270e5f44cf88187618837de09
Ian McLerran <imclerran@protonmail.com>
2024-11-21 11:08:05 -0600

    simplify database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,15 @@
 require('dotenv').config();
 
-module.exports = {
-  "development": {
-    "username": process.env.DEV_DB_USER,
-    "password": process.env.DEV_DB_PASS,
-    "database": process.env.DEV_DB_NAME,
-    "host": process.env.DEV_DB_HOST,
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": process.env.TEST_DB_USER,
-    "password": process.env.TEST_DB_PASS,
-    "database": process.env.TEST_DB_NAME,
-    "host": process.env.TEST_DB_HOST,
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": process.env.PROD_DB_USER,
-    "password": process.env.PROD_DB_PASS,
-    "database": process.env.PROD_DB_NAME,
-    "host": process.env.PROD_DB_HOST,
-    "dialect": "postgres"
-  }
+const config = {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOST,
+  "dialect": process.env.DB_DIALECT,
 }
+
+module.exports = {
+  "development": config,
+  "test": config,
+  "production": config
+};
